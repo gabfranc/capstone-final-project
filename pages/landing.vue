43289a97c07682752276d1c713351b6cad0a85f7
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-04 15:06:54 -0600

    fixing errors

--- pages/landing.vue
+++ pages/landing.vue
@@ -1,12 +1,10 @@
 <script setup>
-  import Hero from "~components/Hero.vue";
   const { data: posts } = await useFetch("/pages/blog", {
     headers: useRequestHeaders(["cookie"]),  
   })
 </script>
 <template>
   <main>
-    <Hero />
 
     
   </main>
