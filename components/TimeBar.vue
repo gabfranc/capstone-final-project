239b4eb9ab0a9c34d136e72c6ef62a03e76431cc
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-04 12:28:06 -0600

    adding time stamp with dayjs

--- components/TimeBar.vue
+++ components/TimeBar.vue
@@ -1,3 +1,28 @@
-<script setup>
+<script>
+//DONE: import dayjs from dayjs
   import dayjs from 'dayjs';
+
+  export default {
+    data() {
+      return {
+        currentTime: '',
+        newTime: ''
+      };
+    },
+    mounted() {
+      this.calculateTime();
+    },
+    methods: {
+      calculateTime() {
+        //DONE: get the current time
+        let currentTime = dayjs();
+       // TODO: adding 1 hour to the current time
+       let newTime = currentTime.add(1, 'hour');
+
+       //TODO: setting the data properties for the time
+       this.currentTime = currentTime.format('YYYY-MM-DD HH:mm:ss');
+       this.newTime = newTime.format('YYYY-MM-DD HH:mm:ss');
+      }
+    }
+  };
 </script>
