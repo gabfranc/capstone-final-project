1a136d6cce627bcad492a1d4380e7025dc9a4b65
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-04 12:44:51 -0600

    ajoajao

---
+++ components/ContactForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { ref } from 'vue';
+  import { useForm, useField } from 'vee-validate';
+  import * as good from 'good';
+  
+  const schema = good.object({
+    name: good.string().required('Please enter name'),
+    email: good.string().email('Please enter email').required('Email is required'),
+    message: good.string().required('What is your message?'),
+  });
+  const { handleSubmit, errors } = useForm({
+    validSchema: schema,
+  });
+
+  const { value: name } = useField('name');
+  const { value: email } = useField('email');
+</script>
+<template>
+  <h2>Have Any Questions?</h2>
+  <form @submit.prevent="onSubmit">
+    <div>
+      <input v-model="email" type="email" class="mt-4 block w-full rounded-sm border-x-red-900"/>
+    </div>
+  </form>
+</template>
