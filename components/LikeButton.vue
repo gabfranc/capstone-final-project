e9e488cbdfc1e313a1fd8155c2016be5957306aa
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-14 00:06:07 -0600

    fixing like button

--- components/LikeButton.vue
+++ components/LikeButton.vue
@@ -1,9 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 
-const likeButtonImg = '../Images/heart.png';
-const dislikeButtonImg = '../Images/unheart.png';
-
+// const likeButtonImg = '../Images/heart.png';
+// const dislikeButtonImg = '../Images/unheart.png';
 
 const isLiked = ref(false);
 
@@ -15,10 +14,11 @@ const toggleLike = () => {
 <template>
   <div>
     <button @click="toggleLike"
-      :class="['sm:ml-48','md:ml-80', 'absolute','text-maroon','bg-yellow', 'hover:bg-orange', 'focus:bg-maroon', 'hover:opacity-80', isLiked ? 'bg-maroon' : 'focus:bg-yellow', 'text-maroon', 'font-bold', 'py-2', 'px-4', 'justify-right', 'rounded']">
+      :class="['sm:ml-48', 'md:ml-80', 'absolute', isLiked ? 'text-yellow' : 'text-maroon', 'bg-yellow', 'hover:bg-orange', 'focus:bg-maroon', 'hover:opacity-80', isLiked ? 'bg-maroon' : 'focus:bg-yellow', 'font-bold', 'py-2', 'px-4', 'justify-right', 'rounded']">
       {{ isLiked ? 'UnLike' : 'Like' }}
     </button>
   </div>
 </template>
+
 
 <!-- absolute top-0 left-0 m-4 text-yellow font-bold hover:italic -->
