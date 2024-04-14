73c7a16d1579362a626f723ed87aaa5676a1ca04
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-14 00:03:26 -0600

    fixing like button

--- components/LikeButton.vue
+++ components/LikeButton.vue
@@ -4,10 +4,9 @@ import { ref } from 'vue';
 const likeButtonImg = '../Images/heart.png';
 const dislikeButtonImg = '../Images/unheart.png';
 
-// Define reactive variables
+
 const isLiked = ref(false);
 
-// Define methods
 const toggleLike = () => {
   isLiked.value = !isLiked.value;
 };
@@ -16,7 +15,7 @@ const toggleLike = () => {
 <template>
   <div>
     <button @click="toggleLike"
-      :class="['sm:ml-48','md:ml-80', 'absolute','bg-yellow', 'hover:bg-orange', 'focus:bg-maroon', 'hover:opacity-80','focus:text-yellow', isLiked ? 'bg-maroon' : 'focus:bg-yellow', 'text-maroon', 'font-bold', 'py-2', 'px-4', 'justify-right', 'rounded']">
+      :class="['sm:ml-48','md:ml-80', 'absolute','text-maroon','bg-yellow', 'hover:bg-orange', 'focus:bg-maroon', 'hover:opacity-80', isLiked ? 'bg-maroon' : 'focus:bg-yellow', 'text-maroon', 'font-bold', 'py-2', 'px-4', 'justify-right', 'rounded']">
       {{ isLiked ? 'UnLike' : 'Like' }}
     </button>
   </div>
